d7964fa6919245b10767caed5e87f1217cda7eb6
Maxim <twiggyramirez111@gmail.com>
2018-07-30 14:44:50 +0300

    module 02 updated

--- js/module-02/js/additional.js
+++ js/module-02/js/additional.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 
 //additional task
 
-const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
-let attempts = 3;
+(() => {
+  let userInput;
+  const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
+  let attempts = 3;
 
-const accBlockedMessage = 'У вас закончились попытки, аккаунт заблокирован!';
-const needPasswordMessage = 'Введите свой пароль';
-const wellcomeMessage = 'Добро пожаловать!';
-const cancelMessage = 'Ну и ладно!';
+  const accBlockedMessage = "У вас закончились попытки, аккаунт заблокирован!";
+  const needPasswordMessage = "Введите свой пароль";
+  const wellcomeMessage = "Добро пожаловать!";
+  const cancelMessage = "Ну и ладно!";
 
-do {
-  if(attempts <= 0) {
+  do {
+    if (attempts <= 0) {
       alert(accBlockedMessage);
       break;
-  }
-  userInput = prompt(needPasswordMessage);
-  if (userInput !== null) {
-    if (passwords.includes(userInput)) {
+    }
+    userInput = prompt(needPasswordMessage);
+    if (userInput !== null) {
+      if (passwords.includes(userInput)) {
         alert(wellcomeMessage);
         break;
-    } else {
+      } else {
         attempts -= 1;
         alert(`Неверный пароль, у вас осталось ${attempts} попыток`);
+      }
+    } else {
+      alert(cancelMessage);
+      break;
     }
-  } else {
-    alert(cancelMessage);
-    break;
-  }
-} while (true);
+  } while (true);
+})();
