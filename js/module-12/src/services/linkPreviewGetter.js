181a25685e261abda531b109c32ba6bfa74c02bd
Maxim <twiggyramirez111@gmail.com>
2018-09-07 19:29:47 +0300

    module 14 updated

--- js/module-12/src/services/linkPreviewGetter.js
+++ js/module-12/src/services/linkPreviewGetter.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import axios from 'axios';
-const key = '5b86632f8cc37dda3dd68bdae57a6f55a9411390cbecc';
+const key = '5b88308dc52943bcc27b5d67121b3987a9840158c7977';
 
 export default function getPreview(url) {
     const apiUrl = `http://api.linkpreview.net/?key=${key}&q=${url}`;
