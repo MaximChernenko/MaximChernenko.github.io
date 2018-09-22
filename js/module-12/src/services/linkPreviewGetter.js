f73428ab98cdbc85e07cc037ec9a369e3afbd8e5
MaximChernenko <twiggyramirez111@gmail.com>
2018-09-22 11:34:36 +0300

    Update linkPreviewGetter.js

--- js/module-12/src/services/linkPreviewGetter.js
+++ js/module-12/src/services/linkPreviewGetter.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 const key = '5b88308dc52943bcc27b5d67121b3987a9840158c7977';
 
 export default function getPreview(url) {
-    const apiUrl = `http://api.linkpreview.net/?key=${key}&q=${url}`;
+    const apiUrl = `https://api.linkpreview.net/?key=${key}&q=${url}`;
     return axios.get(apiUrl);
 }
