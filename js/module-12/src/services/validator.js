dface1be45764c2c687945db303ce3423b074acf
Maxim Chernenko <twiggyramirez111@gmail.com>
2018-09-22 12:49:49 +0300

    module 12 proved

--- js/module-12/src/services/validator.js
+++ js/module-12/src/services/validator.js
@@ -4,7 +4,7 @@ export default class Validator {
     constructor(){}
 
     static get pattern() {
-        return /^https?:\/\/(www\.)?[\.a-zA-Z0-9]+\.[a-z]{2,6}(\.[a-z]{2,6})?\/?(\/[_\/&?=a-zA-Z0-9-]+)?$/;
+        return /^https?:\/\/(www\.)?[\.a-zA-Z0-9]+\.[a-z]{2,6}(\.[a-z]{2,6})?\/?(\/[_\/&?#=a-zA-Z0-9.-]+)?$/;
     }
 
     static isValid(value) {
